cd2b00ae3bb9f926dc930c22dc2906a139661b03
MariosKadriu <marios.kadriu@gmail.com>
2020-02-18 13:58:31 +0200

    Added select movie and seats functionality

---
+++ script.js
@@ -0,0 +1,39 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+// Convert ticket price from string to number by adding the +
+let ticketPrice = +movieSelect.value;
+
+/************** FUNCTIONS **************/
+
+// Update total and count
+const updateSelectedCount = () => {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+};
+
+/************** EVENT LISTENERS **************/
+
+// Movie select event
+movieSelect.addEventListener('change', e => {
+  ticketPrice = +e.target.value;
+
+  updateSelectedCount();
+});
+
+// Seat click event
+container.addEventListener('click', e => {
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
+    e.target.classList.toggle('selected');
+
+    updateSelectedCount();
+  }
+});
